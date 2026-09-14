4e5474fc5fc138d9105790aaaf99331aaaec81b7
SomeRandomDeveloper <git@srd.dev>
2026-09-15 00:08:15 +0200

    Include cloud-core in phan analysis

--- .phan/config.php
+++ .phan/config.php
@@ -3,6 +3,7 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['directory_list'][] = 'vendor/google/cloud-storage';
+$cfg['directory_list'][] = 'vendor/google/cloud-core';
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
